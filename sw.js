3299de27e9baa49050084f494d53ca77fe7052c9
Joanna Huang <joannathhuang@gmail.com>
2020-12-08 16:42:13 -0500

    added assets to service worker to cache shell assets

--- sw.js
+++ sw.js
@@ -1,14 +1,48 @@
+const staticCacheName = "site-static-v2";
+const assets = [
+  "/",
+  "/index.html",
+  "/js/app.js",
+  "/js/ui.js",
+  "/js/materialize.min.js",
+  "/css/styles.css",
+  "/css/materialize.min.css",
+  "/img/dish.png",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+];
+
 // install service worker
 self.addEventListener("install", (e) => {
-  console.log("service worker has been installed");
+  // console.log("service worker has been installed");
+  e.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("caching shell assets");
+      cache.addAll(assets);
+    })
+  );
 });
 
 // activate event
 self.addEventListener("activate", (e) => {
-  console.log("service worker has been activated");
+  // console.log("service worker has been activated");
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCacheName)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
 });
 
 // fetch event
 self.addEventListener("fetch", (e) => {
-  //   console.log("fetch event", e);
+  // console.log("fetch event", e);
+  e.respondWith(
+    caches.match(e.request).then((cacheRes) => {
+      return cacheRes || fetch(e.request);
+    })
+  );
 });
